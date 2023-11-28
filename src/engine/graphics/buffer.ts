893846f22a20d71dafd24f87bc27496a4a939ae2
saile515 <elias.jorgensen2006@gmail.com>
2023-11-28 23:08:18 +0100

    add attribute and uniform implementation

--- src/engine/graphics/buffer.ts
+++ src/engine/graphics/buffer.ts
@@ -2,10 +2,18 @@ export default class Buffer {
     private _buffer: WebGLBuffer;
     private _target: GLenum;
     private _usage: GLenum;
+    readonly components: number;
 
-    constructor(settings: { target: GLenum; usage: GLenum } = { target: gl.ARRAY_BUFFER, usage: gl.STATIC_DRAW }) {
+    constructor(
+        settings: { target: GLenum; usage: GLenum; components: number } = {
+            target: gl.ARRAY_BUFFER,
+            usage: gl.STATIC_DRAW,
+            components: 2,
+        },
+    ) {
         this._target = settings.target;
         this._usage = settings.usage;
+        this.components = settings.components;
 
         this._buffer = gl.createBuffer()!;
     }
@@ -17,5 +25,9 @@ export default class Buffer {
 
     get() {
         return this._buffer;
+    }
+
+    bind() {
+        gl.bindBuffer(this._target, this._buffer);
     }
 }
