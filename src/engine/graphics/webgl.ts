ea6b14773eb5cbe2af2b9d5721f76dffc518eab5
saile515 <elias.jorgensen2006@gmail.com>
2023-11-28 11:36:06 +0100

    init webgl

---
+++ src/engine/graphics/webgl.ts
@@ -0,0 +1,12 @@
+function init_webgl(canvas: HTMLCanvasElement) {
+    const gl = canvas.getContext("webgl2");
+
+    if (!gl) {
+        alert("This browser does not support WebGL2.");
+        return 1;
+    }
+
+    const global = globalThis || window;
+
+    global.gl = gl;
+}
