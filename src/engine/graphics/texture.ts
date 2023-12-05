a49a419d214a5de22491a3decf68a7c9288b0218
saile515 <elias.jorgensen2006@gmail.com>
2023-12-05 20:05:31 +0100

    add use function

--- src/engine/graphics/texture.ts
+++ src/engine/graphics/texture.ts
@@ -16,7 +16,8 @@ export default class Texture {
 
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     }
 
     async init() {
@@ -38,5 +39,10 @@ export default class Texture {
                 return Promise.reject("Image could not be loaded");
             });
         }
+    }
+
+    use() {
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this._texture);
     }
 }
