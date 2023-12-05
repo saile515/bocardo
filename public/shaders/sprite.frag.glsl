5d1b994d62fb34b60d4711d0c0b6987e09c528a2
saile515 <elias.jorgensen2006@gmail.com>
2023-12-05 20:07:17 +0100

    update assets

--- public/shaders/sprite.frag.glsl
+++ public/shaders/sprite.frag.glsl
@@ -1,3 +1,7 @@
+varying highp vec2 _texture_position;
+
+uniform sampler2D sampler;
+
 void main(void) {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = texture2D(sampler, _texture_position);
 }
--- public/shaders/sprite.vert.glsl
+++ public/shaders/sprite.vert.glsl
@@ -1,5 +1,9 @@
 attribute vec2 vertex_position;
+attribute vec2 texture_position;
+
+varying highp vec2 _texture_position;
 
 void main(void) {
     gl_Position = vec4(vertex_position, 0.0, 1.0);
+    _texture_position = texture_position;
 }
