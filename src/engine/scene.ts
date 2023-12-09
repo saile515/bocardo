d2084220e4cb6c96383207450304a51c69ec11bb
saile515 <elias.jorgensen2006@gmail.com>
2023-12-09 15:37:12 +0100

    move render logic to scene

--- src/engine/scene.ts
+++ src/engine/scene.ts
@@ -1,10 +1,33 @@
 import ECS from "./ecs/ecs";
-import { init_webgl } from "./graphics/webgl.ts";
+import Sprite from "./components/sprite.ts";
+import Camera from "./components/camera.ts";
+import { init_webgl, clear } from "./graphics/webgl.ts";
 
 export default class Scene {
+    private _active_camera: Camera | null = null;
     readonly ecs = new ECS();
 
     constructor(canvas: HTMLCanvasElement) {
         init_webgl(canvas);
+    }
+
+    set_active_camera(camera: Camera) {
+        this._active_camera = camera;
+    }
+
+    draw() {
+        clear();
+
+        if (!this._active_camera || !Sprite.shader) {
+            return;
+        }
+
+        Sprite.shader.use();
+
+        Sprite.shader.set_uniform_matrix("vp_matrix", this._active_camera.projection_matrix as Float32Array, 3);
+
+        this.ecs.query<[Sprite]>([Sprite]).forEach(([sprite]) => {
+            sprite.draw();
+        });
     }
 }
--- src/game/main.ts
+++ src/game/main.ts
@@ -1,16 +1,18 @@
 import Sprite from "../engine/components/sprite.ts";
-import { clear } from "../engine/graphics/webgl.ts";
+import Camera from "../engine/components/camera.ts";
 import Scene from "../engine/scene.ts";
 
 export async function init_game() {
-    const scene = new Scene(document.getElementById("game_canvas") as HTMLCanvasElement);
+    const canvas = document.getElementById("game_canvas") as HTMLCanvasElement;
+    const scene = new Scene(canvas);
+    const [camera] = scene.ecs.create_entity([new Camera(canvas.width, canvas.height)]);
+
+    scene.set_active_camera(camera);
 
     scene.ecs.create_entity([new Sprite("/images/amogus.png")]);
 
     function render() {
-        clear();
-
-        scene.ecs.query<[Sprite]>([Sprite]).forEach((entity) => entity[0].draw());
+        scene.draw();
 
         requestAnimationFrame(render);
     }
