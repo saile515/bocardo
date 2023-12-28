aafad186619cf7a88c75dc40141e7844f9ee383b
saile515 <elias.jorgensen2006@gmail.com>
2023-12-28 11:35:54 +0100

    merge base

--- public/shaders/sprite.vert.glsl
+++ public/shaders/sprite.vert.glsl
@@ -1,10 +1,7 @@
 #version 300 es
 
 uniform highp mat3 vp_matrix;
-<<<<<<< HEAD
 uniform highp mat3 model_matrix;
-=======
->>>>>>> 7662b0c0904f72665c7d4fdf8e6152260d149452
 
 out highp vec2 texture_position;
 
@@ -13,10 +10,6 @@ void main(void) {
     vertex_position.x = floor(float(gl_VertexID / 2)) - 0.5;
     vertex_position.y = mod(float(gl_VertexID), 2.0) - 0.5;
 
-<<<<<<< HEAD
     gl_Position = vec4((vp_matrix * model_matrix * vec3(vertex_position, 1.0)).xy, 0.0, 1.0);
-=======
-    gl_Position = vec4(vp_matrix * vec3(vertex_position, 0.0), 1.0);
->>>>>>> 7662b0c0904f72665c7d4fdf8e6152260d149452
     texture_position = vertex_position + vec2(0.5);
 }
